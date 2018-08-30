7964d3f9bc39d528e538225c32f8c0bcb592ddce
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-30 14:29:43 -0400

    add jquery extend

---
+++ scripts/jquery-extend.js
@@ -0,0 +1,8 @@
+$.fn.extend({
+  serializeJson: function() {
+    const formData = new FormData(this[0]);
+    const o = {};
+    formData.forEach((val, name) => o[name] = val);
+    return JSON.stringify(o);
+  }
+});
